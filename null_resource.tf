cc0d811a6c8669161407860361a2eb6259a7fe5b
kenvpt <49701513+kenvpt@users.noreply.github.com>
2020-05-31 17:54:24 -0500

    Update null_resource.tf

--- null_resource.tf
+++ null_resource.tf
@@ -9,7 +9,7 @@ resource "null_resource" "gitlab" {
   }
   
     inline = [
-          "sudo yum update",
+          "sudo yum update -y",
           "sudo yum install wget unzip -y",
           "sudo wget -O splunk-8.0.3-a6754d8441bf-Linux-x86_64.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.0.3&product=splunk&filename=splunk-8.0.3-a6754d8441bf-Linux-x86_64.tgz&wget=true'",
           "sudo tar xvzf splunk-8.0.3-a6754d8441bf-Linux-x86_64.tgz -C /opt",
